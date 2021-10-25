cda5c584eb7fac81ebcd3c4dfcdf5d0d034218c4
Teseo15 <brayan.blas.egaovil30@gmail.com>
2021-10-25 17:07:20 -0500

    Agregar archivos de proyecto.

---
+++ Lab11/Lab11/View/Operations.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab11.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operations : ContentPage
+    {
+        public Operations()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModel.OperationsViewModel();
+        }
+    }
+}
